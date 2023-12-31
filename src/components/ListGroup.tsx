e4ae0b4149041b18ff9e49f0cfa8284ee0864edb
Kenneth Camacho <Kennethcxv@gmail.com>
2023-12-31 12:41:06 -0500

    Creating a List Group

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,13 @@
+function ListGroup() {
+  return (
+    <ul className="list-group">
+      <li className="list-goup-item">An item</li>
+      <li className="list-goup-item">A Second Item</li>
+      <li className="list-goup-item">A Third Item</li>
+      <li className="list-goup-item">A fourth Item</li>
+      <li className="list-goup-item">And a fifth One</li>
+    </ul>
+  );
+}
+
+export default ListGroup;
