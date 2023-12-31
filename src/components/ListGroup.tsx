e2cba87561c73c45f205ff13554ec67bf30c7be0
Kenneth Camacho <Kennethcxv@gmail.com>
2023-12-31 14:19:31 -0500

    Managing States

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,14 +1,30 @@
+import { useState } from "react";
+
 function ListGroup() {
   let items = ["New York", "San Francisco", "London", "Paris"];
-  items = [];
+  // Hook(Function that allows to get built in functions in react)
+  const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <>
       <h1>List</h1>
       {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li>
+        {items.map((item, index) => (
+          <li
+            //Looks at if the selected index value matches the index value. if it does then it does the list group and highlights(active) else it just does list group
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </>
